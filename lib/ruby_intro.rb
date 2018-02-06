25d3b39c21efd4aba5ea6a56b1255d877eb044e1
henrysurface <31938095+henrysurface@users.noreply.github.com>
2018-02-05 19:29:53 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,48 +4,87 @@
 
 def sum arr
   # YOUR CODE HERE
-   arrr.each do |i|
-    sum = sum + i
-  end
-  if sum == null
-    return 0
-  else
-    return sum
-  end
+  arr.inject(0){|x,y| x + y}
 end
+
 
 def max_2_sum arr
   # YOUR CODE HERE
-   temp = arr.sort
-  if arr[-1] == null && arr[-2] == null
+  if arr.length == 0
   	return 0
-  elsif arr[-2] == null
-  	return arr[-1]
+  elsif arr.length == 1
+  	return arr[0]
   else
-  	return arr[-1] + arr[-2]
-  end 
+  	return arr.max(2).inject(0, :+)
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  size = arr.length
+  if size < 2
+  	return false
+  end
+  for i in 0...(size-1) do
+  	j = i + 1
+  	sum = arr[i] + arr[j]
+  	if sum == n
+  		return true
+  	end
+  end
+  return false
+ 
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  ca = s.capitalize
+  if /^[^aeiou\W]/i.match(ca[0]) == nil
+  	return false
+  end
+  return true
+
 end
+
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if /[a-zA-Z^$2-9*]/.match(s)
+  	return false
+  end
+  if /^[10]*00$/.match(s)
+  	return true
+  elsif s == "0"
+  	return true
+  else
+  	return false
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+	attr_accessor :isbn, :price
+	def initialize isbn, price
+		if isbn.empty? || price <= 0
+			raise ArgumentError
+		end
+
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		p1 = '%.2f' % price.to_f
+		return "$"+"#{p1}"
+	end
 end
+
